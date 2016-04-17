fd51433757526924d495c0fba86652271b77fe49
root <root@xiachsh21.eng.platformlab.ibm.com>
2016-04-17 10:49:50 -0400

    add avl header

---
+++ avl.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+typedef struct _avlNode
+{
+	int val;
+	struct _avlNode * left;
+	struct _avlNode * right;
+} avlNode,* avlTree;
+
+
+int main(int argc,char ** argv)
+{
+
+
+	
+	return 0;
+}
