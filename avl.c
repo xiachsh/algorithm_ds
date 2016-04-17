c86b3bdafae4390cabc8a9d62d1741ddb7c8c9aa
xiachsh <lotrxia@163.com>
2016-04-17 11:32:43 -0400

    sleepy,

--- avl.c
+++ avl.c
@@ -9,14 +9,29 @@ typedef struct _avlNode
 	int val;
 	struct _avlNode * left;
 	struct _avlNode * right;
+	int bf;
 } avlNode,* avlTree;
 
 
+void avl_destroy(avlTree * tree)
+{
+	if (*tree)
+	{
+		avl_destroy( &((*tree)->left));
+		(*tree)->left = NULL;
+		
+		avl_destroy( &((*tree)->right));
+		(*tree)->right = NULL;
+		
+		free(*tree);
+		*tree = NULL;
+	}
+}
+
 void avl_init(avlTree * tree)
 {
-
-
-
+	avl_destroy(tree);	
+	*tree = NULL;
 }
 
 void left_rotate(avlTree * tree)
@@ -33,8 +48,33 @@ void right_rotate(avlTree * tree)
 
 void avl_insert(avlTree * tree,int val)
 {
+	if (*tree)
+	{
+		if ((*tree)->val > val)
+			avl_insert(&((*tree)->left),val);
+		else if ((*tree)->val < val)
+			avl_insert(&((*tree)->right),val);
+		else
+			printf("Warning: Duplicate key %d\n",val);
+	}	
+	else
+	{
+		avlNode * tmp = (avlNode* ) malloc(sizeof(avlNode));
+		tmp->val = val;
+		tmp->left = tmp->right = NULL;
+		*tree = tmp;
+	}
+}
 
 
+void avl_preorder_tranversal(avlTree * tree)
+{
+	if (*tree)
+	{
+		printf("Address: 0x%lx\tVal: %d\tLeft: 0x%lx\tRight: 0x%lx\n",*tree,(*tree)->val,(*tree)->left,(*tree)->right);
+		avl_preorder_tranversal(&((*tree)->left));
+		avl_preorder_tranversal(&((*tree)->right));
+	}	
 }
 
 void avl_delete(avlTree * tree,int val)
@@ -46,14 +86,25 @@ void avl_delete(avlTree * tree,int val)
 
 int node_height(avlTree * tree,avlNode node)
 {
+	if (*tree)
+	{
 
+	}
 
 }
 
 int main(int argc,char ** argv)
 {
-
-
+	int elements = 10;
+	int i = 0;
+	avlTree t = NULL;
 	
+
+	avl_init(&t);
+	
+	for (i=0;i<elements;i++)
+		avl_insert(&t,rand() % 100);
+	avl_preorder_tranversal(&t);
+	avl_destroy(&t);
 	return 0;
 }
