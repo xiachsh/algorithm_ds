c72d654f4ba9d6d7bd514f146e95ecf24107be73
xiachsh <lotrxia@163.com>
2016-07-01 23:41:16 +0800

    add lcs.c

---
+++ lcs.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int lcs(char * str1,char * str2)
+{
+
+
+	return 0;
+}
+
+
+int main(int argc,char argv)
+{
+
+
+
+}
+
